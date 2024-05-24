d4796824b060010b289be43ff1400bf44931abba
ChrizDope <incubusid@googlemail.com>
2024-05-24 15:24:52 +0200

    Add Files

---
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerCharacterTest {
+
+    @Test
+    void getX_shouldReturn0_whenCalled(){
+        int expected = 0;
+        assertEquals(expected, 0);
+    }
+
+}
