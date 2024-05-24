04752f1987b07591e75e7be49c0132fa2907e15d
ChrizDope <incubusid@googlemail.com>
2024-05-24 15:26:54 +0200

    Correct existing files

--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -9,7 +9,14 @@ class PlayerCharacterTest {
     @Test
     void getX_shouldReturn0_whenCalled(){
         int expected = 0;
-        assertEquals(expected, 0);
+        assertEquals(expected, PlayerCharacter.getX());
     }
+
+    @Test
+    void getY_shouldReturn0_whenCalled(){
+        int expected = 0;
+        assertEquals(expected, PlayerCharacter.getY());
+    }
+
 
 }
