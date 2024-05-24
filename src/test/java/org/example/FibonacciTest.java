6ee01a73939fc38a7700e435b8d8b900dc8f9fff
ChrizDope <incubusid@googlemail.com>
2024-05-24 16:43:39 +0200

    Added Fibonacci Files

--- src/test/java/org/example/FibonacciTest.java
+++ src/test/java/org/example/FibonacciTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciTest {
-    private int x = 0, y = 1;
 
     @Test
     void testFibonacci_shouldBeFibAdded_when4Runs() {
